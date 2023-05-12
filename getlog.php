6a105481820ffee92c51a0bbe750533fe930fd00
DarkDuli <103023323+DarkDuli@users.noreply.github.com>
2023-05-12 10:55:00 +0200

    Add files via upload

--- getlog.php
+++ getlog.php
@@ -1,6 +1,6 @@
 <?php
 require 'db_connection.php';
-$username = $_POST['nickname'];
+$username = $_POST['username'];
 $password = $_POST['password'];
 
 $sql="SELECT * FROM users WHERE username='$username'";
@@ -9,7 +9,7 @@ if($result->num_rows == 0){
     echo "L' username utilizzato non è registrato";
 }else{
     $password=md5($password);
-    $sql="SELECT * FROM users WHERE nickname = '$username' AND password = '$password'";
+    $sql="SELECT * FROM users WHERE username = '$username' AND password = '$password'";
     $result=$conn->query($sql);
     if($result->num_rows>0){
         session_start();
